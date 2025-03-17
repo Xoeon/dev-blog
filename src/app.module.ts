233edcd6d58dc6fecfd0c46a0c7c6755dc53b39e
soyeon <jayy_19@ewhain.net>
2025-03-18 00:09:33 +0900

    Feat: Implement environment variable management with dotenv and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,24 @@ import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        retryAttempts: 10,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        retryAttempts: configService.get('NODE_ENV') === 'prod' ? 10 : 1,
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        database: 'oxlip',
-        username: 'root',
-        password: '00000000',
-        entities: [path.join(__dirname, '/entities/**/*.entity.{js, ts}')],
+        host: configService.get('DB_HOST'),
+        port: Number(configService.get('DB_PORT')),
+        database: configService.get('DB_NAME'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        entities: [path.join(__dirname, 'src/entities/**/*.entity.{js, ts}')],
         synchronize: false,
         logging: true,
         timezone: 'local',
