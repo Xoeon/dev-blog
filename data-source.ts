242c4f787b326718c42032c74a7ac9493494943a
soyeon <jayy_19@ewhain.net>
2025-03-17 23:38:42 +0900

    Feat: Add TypeOrmModule import for database configuration

---
+++ data-source.ts
@@ -0,0 +1,17 @@
+import * as path from 'path';
+import { DataSource } from 'typeorm';
+
+export const dataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  database: 'posts',
+  username: 'root',
+  password: '00000000',
+  entities: [
+    path.join(__dirname, 'src/entities/**/*.entity.ts'),
+    path.join(__dirname, 'dist/entities/**/*.entity.js'),
+  ],
+  synchronize: false,
+  logging: true,
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        retryAttempts: 10,
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        database: 'posts',
+        username: 'root',
+        password: '00000000',
+        entities: [path.join(__dirname, '/entities/**/*.entity.{js, ts}')],
+        synchronize: false,
+        logging: true,
+        timezone: 'local',
+      }),
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
